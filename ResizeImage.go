825186aa0da6251b1e431c3134b1e187203b3ad9
Carlos <juyut21@gmail.com>
2021-06-30 16:49:21 +0200

    First commit
    First commit

---
+++ ResizeImage.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/nfnt/resize"
+	"image/jpeg"
+	"log"
+	"os"
+)
+
+func main() {
+	// open "test.jpg"
+	file, err := os.Open("test.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode jpeg into image.Image
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	// resize to 64x64 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(64, 64, img, resize.Lanczos3)
+
+	out, err := os.Create("test_resized.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	jpeg.Encode(out, m, nil)
+}
